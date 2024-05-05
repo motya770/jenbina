a9ddd4396b45bddac4d76d6619f3450a93042be9
Matthew Kudelin <motya770@gmail.com>
2024-05-06 01:29:11 +0300

    adding LLM

--- engine/enigine.py
+++ engine/enigine.py
@@ -1,9 +1,10 @@
 from model.actor import Actor
-
+from openai import OpenAI
 
 class Engine: 
     def __init__(self, actor, memory):
         self.actor = Actor(name="Scarlett")
+        self.client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
     
     def run(self):
 
@@ -13,8 +14,38 @@ class Engine:
         print(self.actor)
         print(self.memory)
 
-    def llm_neocortext():
-        pass
+    def llm_neocortex(self):
+        completion = self.client.chat.completions.create(
+        model="lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF",
+        messages=[
+            {"role": "system", "content": """You are part of the brain known as neocortex 
+            - you will translate desires and motivations of the agent into 
+            actions that are going to be available for you.
+            You are going to communicate with environment - 
+            the description of your surroundings are going to be provided.
+            Also you will communicate with another human. Stop generating after 100 words. """},
+            {"role": "user", "content": "Agent status:  Enviromement: "}
+        ],
+        temperature=0.7,
+        )
 
-    def llm_world():
-        pass
+        print(completion.choices[0].message)
+        return completion.choices[0].message
+
+    def llm_world(self):
+        completion = self.client.chat.completions.create(
+        model="lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF",
+        messages=[
+            {"role": "system", "content": """You are representation of the world 
+            or eniroment inside which or smart agent are going to live, 
+            you can describe to him how his house looks inside for example. 
+            Also you will generate description of roads, food, restaurants,
+            city - everything that we see in everyday live. 
+            The agent will travel inside that city and will perform different tasks inside. """},
+            {"role": "user", "content": "Introduce yourself."}
+        ],
+        temperature=0.7,
+        )
+
+        print(completion.choices[0].message)
+        return completion.choices[0].message
